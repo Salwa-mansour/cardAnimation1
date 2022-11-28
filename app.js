538f5918ee3358f5f009c315e1f20511a8c8d8f1
Salwa-mansour <alqadasisal@gmail.com>
2022-11-28 10:03:12 +0300

    adding animation class

---
+++ app.js
@@ -0,0 +1,34 @@
+// var deviceWidth=document.documentElement.clientWidth;
+// window.addEventListener("orientationchange",
+//  function () {
+//  deviceWidth = document.documentElement.clientWidth;
+
+// })
+
+// const cards = document.getElementsByClassName("card-container")
+
+// const cardsArr= Array.from(cards)
+
+
+//     absorvingCards(cardsArr)
+
+// // --------------------------------------------------------
+// function absorvingCards(targetArr){
+//     const observer = new IntersectionObserver(
+//         entries =>{
+//         entries.forEach(entry=>{
+//             entry.target.classList.toggle('animated',entry.isIntersecting)
+//             console.log("dfsdf")
+//         })
+//     }
+//         ,{
+//             threshold:1,
+//             // rootMargin:"-30px"
+//         }
+       
+//     )
+//     targetArr.forEach(card=>{
+//         observer.observe(card)
+//     })
+    
+// }
